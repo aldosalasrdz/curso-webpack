6f5c22f63d5d16d91623c47ea609b9feb6b56977
Aldo Salas <aldosalasrdz@gmail.com>
2022-06-26 06:18:32 -0500

    Optimización: hashes, compresión y minificación de archivos

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 /* const CopyPlugin = require('copy-webpack-plugin') */
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 const ruleForStyles = {
   test: /\.css|\.styl$/i,
@@ -28,15 +30,15 @@ const ruleForFonts = {
   test: /\.(woff|woff2|eot|ttf|otf)$/i,
   type: 'asset/resource',
   generator: {
-    filename: 'assets/fonts/[hash][ext]'
+    filename: 'assets/fonts/[name].[contenthash].[ext]'
   }
 }
 
 module.exports = {
-  entry: './src/index.js',
+  // entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/images/[hash][ext]'
   },
   resolve: {
@@ -56,6 +58,15 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html'
     }),
-    new MiniCssExtractPlugin()
-  ]
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin()
+    ]
+  }
 }
